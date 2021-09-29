9afb0d6e8687a738e832e2cbf99c3cd5ee60ca88
Michael Howitz <mh@gocept.com>
2021-09-29 08:13:39 +0200

    Back to development: 2.2.1

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ else:
 
 setup(
     name='zodbpickle',
-    version='2.2.0',
+    version='2.2.1.dev0',
     description='Fork of Python 2 and 3 pickle module.',
     author='Python and Zope Foundation',
     author_email='zodb-dev@zope.org',
