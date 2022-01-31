a4c1902ce7f6f2fb80284e33ed8c3b8bc17299f6
Michael Howitz <mh@gocept.com>
2022-01-31 08:34:53 +0100

    Add support for Python 3.11.0a4.
    This requires the changes from https://github.com/zopefoundation/zodbpickle/pull/64
    to run successfully.

--- .manylinux-install.sh
+++ .manylinux-install.sh
@@ -28,13 +28,15 @@ yum -y install libffi-devel
 
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
-    if [[ "${PYBIN}" == *"cp27"* ]] || \
+    if \
+       [[ "${PYBIN}" == *"cp27"* ]] || \
        [[ "${PYBIN}" == *"cp35"* ]] || \
+       [[ "${PYBIN}" == *"cp311"* ]] || \
        [[ "${PYBIN}" == *"cp36"* ]] || \
        [[ "${PYBIN}" == *"cp37"* ]] || \
        [[ "${PYBIN}" == *"cp38"* ]] || \
-       [[ "${PYBIN}" == *"cp310"* ]] || \
-       [[ "${PYBIN}" == *"cp39"* ]]; then
+       [[ "${PYBIN}" == *"cp39"* ]] || \
+       [[ "${PYBIN}" == *"cp310"* ]] ; then
         "${PYBIN}/pip" install -e /io/
         "${PYBIN}/pip" wheel /io/ -w wheelhouse/
         if [ `uname -m` == 'aarch64' ]; then
--- CHANGES.rst
+++ CHANGES.rst
@@ -5,7 +5,7 @@
 2.2.1 (unreleased)
 ==================
 
-- Nothing changed yet.
+- Add support for Python 3.11 (as of 3.11.0a4).
 
 
 2.2.0 (2021-09-29)
