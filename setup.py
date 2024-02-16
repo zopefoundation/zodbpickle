3cde1d2155e6833a879cab8bb58cae0d71700133
Michael Howitz <icemac@gmx.net>
2024-02-16 08:37:44 +0100

    Back to development: 3.3

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ else:
 
 setup(
     name='zodbpickle',
-    version='3.2',
+    version='3.3.dev0',
     description='Fork of Python 3 pickle module.',
     author='Python and Zope Foundation',
     author_email='zodb-dev@zope.dev',
